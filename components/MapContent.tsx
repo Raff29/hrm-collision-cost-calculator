d3997e117ae56018bc60b67c0475ca82b47f1a76
Raphael Pinto <raphaelllpinto@gmail.com>
2025-03-03 12:41:22 -0400

    added bounds restrictions and limited zoom out

--- components/MapContent.tsx
+++ components/MapContent.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import DrawMap from "./DrawMap";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 import L from "leaflet";
 import "leaflet-draw";
-
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
@@ -23,14 +17,21 @@ L.Icon.Default.mergeOptions({
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
 });
 
-
 export default function MapContent() {
+  const bounds = L.latLngBounds([
+    [44.4500, -64.3000], 
+    [45.0500, -63.0000], 
+  ]);
+
   return (
     <MapContainer
       center={[44.65, -63.57]}
       zoom={12}
       scrollWheelZoom={true}
       style={{ height: "400px", width: "100%" }}
+      minZoom={10}
+      maxBounds={bounds}
+      maxBoundsViscosity={1.0}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
