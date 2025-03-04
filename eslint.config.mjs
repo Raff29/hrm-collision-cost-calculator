8b28671c2ea0d13dee82f4d990d50cccb11f2a97
Raphael Pinto <raphaelllpinto@gmail.com>
2025-03-04 13:27:16 -0400

    change litting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": process.env.NODE_ENV === "production" ? ["error", { allow: ["warn", "error"] }] : "off"
+    }
+  }
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 compiler: {
-  removeConsole: process.env.NODE_ENV === "production" ? {exclude: ["error", "warn"]} : false,
 }};
 
 export default nextConfig;
