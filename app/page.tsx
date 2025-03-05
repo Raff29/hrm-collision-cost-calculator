d0053b19dc0f5c4c07e1a68c5b855f7b2deae4ed
Raphael Pinto <raphaelllpinto@gmail.com>
2025-03-05 16:03:20 -0400

    refactor: replace MapWithNoSRR and CostAnalysis with CollisionDataProvider in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import MapWithNoSRR from "@/components/MapWithNoSRR";
-import CostAnalysis from "@/components/CostAnalysis";
 import AboutCalculation from "@/components/AboutCalculation";
+import CollisionDataProvider from "@/components/CollisionDataProvider";
 
 export default function Home() {
   return (
@@ -9,8 +8,7 @@ export default function Home() {
         <h1 className="text-3xl font-bold mb-8 text-center text-hrm-blue">
           HRM Vehicle Collision Cost Estimator
         </h1>
-        <MapWithNoSRR />
-        <CostAnalysis />
+        <CollisionDataProvider />
         <AboutCalculation />
       </div>
     </main>
