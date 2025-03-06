4c838a5c353954928ac694fbf53a038479564717
Raphael Pinto <60246144+Raff29@users.noreply.github.com>
2025-03-05 22:32:51 -0400

    fix: correct formatting and syntax in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { FaGithub, FaEnvelope } from "react-icons/fa"
+import { FaGithub, FaEnvelope } from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -16,12 +16,14 @@ export default function Footer() {
           >
             <FaGithub className="w-6 h-6" />
           </a>
-          <a href="mailto:raphael.pinto1@outlook.com" className="hover:text-hrm-light transition-colors">
+          <a
+            href="mailto:raphael.pinto1@outlook.com"
+            className="hover:text-hrm-light transition-colors"
+          >
             <FaEnvelope className="w-6 h-6" />
           </a>
         </div>
       </div>
     </footer>
-  )
+  );
 }
-
