2597b4044d405e3e245addd29f3079d9ed8b198c
Raphael Pinto <raphaelllpinto@gmail.com>
2025-03-07 15:57:03 -0400

    refactor CollisionDataProvider for improved code consistency and readability

--- components/CollisionDataProvider.tsx
+++ components/CollisionDataProvider.tsx
@@ -1,30 +1,33 @@
-"use client"
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 import MapWithNoSRR from "./MapWithNoSRR";
-import CostAnalysis from './CostAnalysis';
-import { CollisionCostData } from '@/lib/services/costCalculationService';
+import CostAnalysis from "./CostAnalysis";
+import { CollisionCostData } from "@/lib/services/costCalculationService";
 
 export default function CollisionDataProvider() {
-    const [collisionData, setCollisionData] = useState<CollisionCostData| null>(null);
-    const [selectedYear, setSelectedYear] = useState<number>(new Date().getFullYear());
+  const [collisionData, setCollisionData] = useState<CollisionCostData | null>(
+    null
+  );
+  const [selectedYear, setSelectedYear] = useState<number>(
+    new Date().getFullYear()
+  );
 
-    const handleCollisionDataChange = (data: CollisionCostData) => {
-        setCollisionData(data);
-    };
+  const handleCollisionDataChange = (data: CollisionCostData) => {
+    setCollisionData(data);
+  };
 
-    const handleYearChange = (year: number) => {
-        setSelectedYear(year);
-    };
+  const handleYearChange = (year: number) => {
+    setSelectedYear(year);
+  };
 
-    return (
-        <>
-        <MapWithNoSRR
+  return (
+    <>
+      <MapWithNoSRR
         onCollisionDataChange={handleCollisionDataChange}
         onYearChange={handleYearChange}
-        />
-        <CostAnalysis year={selectedYear} costData={collisionData} />
-        </>
-    )
-
+      />
+      <CostAnalysis year={selectedYear} costData={collisionData} />
+    </>
+  );
 }
