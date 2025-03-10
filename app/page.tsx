3532228632ecef13fa5833473461d354ce68b3cc
Raphael Pinto <raphaelllpinto@gmail.com>
2025-03-10 10:57:25 -0300

    delete title from Home component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main className="flex-grow">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-8 text-center text-hrm-blue">
-          HRM Vehicle Collision Cost Estimator
-        </h1>
         <CollisionDataProvider />
       </div>
     </main>
